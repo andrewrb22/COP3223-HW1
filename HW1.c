e0d6ab00eae2f097bfc8eec2290380eabf7b7bb0
Andrew Rodriguez <andrewrb22@gmail.com>
2022-01-17 18:30:03 -0500

    Answer question 1, 2 and 3

--- HW1.c
+++ HW1.c
@@ -1,15 +1,23 @@
+//Answer to question 1 
+//It’s 
+// ormal to        ake C! 1o you like it!
+//----------------------------------
+//Answer to question 2 
+// 3 , 0.00 , 1.50 , 1 2 
+//----------------------------------
+//Answer to question 3 
+// $  36
+//----------------------------------
+//Answer to question 4 
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
-    int x;
-
-    printf("Enter a number: ");
-
-    scanf("%d", &x);
-
-    printf("You're number times 2 is:  %d", 2*x);
-
+    char myChar = '$'; 
+    printf ("%c   %d\n" , myChar , myChar);
     return 0;
 }
+
+
